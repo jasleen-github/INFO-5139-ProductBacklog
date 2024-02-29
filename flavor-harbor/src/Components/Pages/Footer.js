43d1ea2ef5a98f2a2ba1cb7421d76c906caffccd
Jasleen Kaur <ijasleenkaur59@gmail.com>
2024-02-29 18:33:51 -0500

    firebase added

--- flavor-harbor/src/Components/Pages/Footer.js
+++ flavor-harbor/src/Components/Pages/Footer.js
@@ -1,5 +1,3 @@
-// src/components/Footer.js
-
 import React from "react";
 
 const Footer = () => {
---
+++ flavor-harbor/src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAu72jVwgzRF4UDKJnc2yJu3nXwRFGo1UE",
+  authDomain: "flavorharbor-3f0f9.firebaseapp.com",
+  projectId: "flavorharbor-3f0f9",
+  storageBucket: "flavorharbor-3f0f9.appspot.com",
+  messagingSenderId: "327958294565",
+  appId: "1:327958294565:web:a9316dacd5df3a65354fcc",
+  measurementId: "G-W2BPFSD03C",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
