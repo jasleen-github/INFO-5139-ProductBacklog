110dec31faf8ecd959d2438a55c6833ac9e4509f
Harpreet7011 <kaur.harpreet030503@gmail.com>
2024-03-04 19:28:10 -0500

    ff

--- flavor-harbor/src/App.js
+++ flavor-harbor/src/App.js
@@ -1,6 +1,12 @@
+<<<<<<< Updated upstream
 // App.js
 
 import React, { useState } from "react";
+=======
+
+import React from "react";
+import Login from "./Components/Pages/LoginPage";
+>>>>>>> Stashed changes
 import Header from "./Components/Pages/Header";
 import Footer from "./Components/Pages/Footer";
 import Login from "./Components/Pages/LoginPage";
