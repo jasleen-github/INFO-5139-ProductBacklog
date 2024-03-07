bf5271fe34b9b553bf237b65e9669a9c8897c317
Jasleen Kaur <ijasleenkaur59@gmail.com>
2024-03-07 12:48:26 -0500

    added routes

--- flavor-harbor/src/App.js
+++ flavor-harbor/src/App.js
@@ -5,13 +5,6 @@ import React, { useState, useEffect } from "react";
 import Header from "./Components/Pages/Header";
 import Footer from "./Components/Pages/Footer";
 import UserProfile from "./Components/Pages/UserProfile";
-import { auth } from "./firebaseConfig"; // Import Firebase authentication
-import { signInWithEmailAndPassword } from "firebase/auth"; // Import Firebase auth methods
-import {
-  getUserData,
-  getFavoriteRecipes,
-  getRecentlyViewedRecipes,
-} from "./api"; // Import functions to fetch user data and recipes
 
 const App = () => {
   return (
